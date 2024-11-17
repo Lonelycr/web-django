ba653a4c05c975d808321d5cd7163e3a31733c45
Lonelycr <camilodotes503@gmail.com>
2024-11-17 19:50:37 -0300

    Update settings.py

--- canchas/settings.py
+++ canchas/settings.py
@@ -67,15 +67,8 @@ WSGI_APPLICATION = 'canchas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'canchas',
-        'USER' : 'root',
-        'PASSWORD' : '',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
-        'OPTIONS' : {
-            'init_command':'SET sql_mode="STRICT_ALL_TABLES"',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
